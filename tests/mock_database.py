788c8b1f5859d97db750b6584e91aaae8cf5aef9
Axel <edgysquirrel@pm.me>
2021-08-14 09:35:26 +0200

    tests: Refactored database connection variables to outside scope of mock database class.

--- tests/mock_database.py
+++ tests/mock_database.py
@@ -5,35 +5,46 @@ from sqlalchemy import MetaData, text
 from ceviche.storage.database import Database, DatabaseCredential
 from ceviche.storage.ddriver import DatabaseClient, DatabaseDriver, DDriver
 
+DB_HOST = getenv("POSTGRES_HOST")
+DB_NAME = getenv("POSTGRES_DB")
+DB_PWRD = getenv("POSTGRES_PWRD")
+DB_USER = getenv("POSTGRES_USER")
+
+DDRIVER = DDriver(client=DatabaseClient.postgresql, driver=DatabaseDriver.psycopg2)
+CRED = DatabaseCredential(dbhost=DB_HOST, dbname=DB_NAME, dbpwrd=DB_PWRD, dbuser=DB_USER)
+META = MetaData()
+DB = Database(DDRIVER, CRED, META)
+ENGINE = DB.gen_engine()
+
+MOCK_DB_NAME = "mock_database"
+
 
 class MockDatabase(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        dbhost = getenv("POSTGRES_HOST")
-        dbname = getenv("POSTGRES_DB")
-        dbpwrd = getenv("POSTGRES_PWRD")
-        dbuser = getenv("POSTGRES_USER")
 
-        ddriver = DDriver(client=DatabaseClient.postgresql, driver=DatabaseDriver.psycopg2)
-        meta = MetaData()
-        cred = DatabaseCredential(dbhost=dbhost, dbname=dbname, dbpwrd=dbpwrd, dbuser=dbuser)
-        db = Database(ddriver, cred, meta)
-        engine = db.gen_engine()
+        conn = ENGINE.connect()
 
-        mock_db_name = "mock_database"
-
-        conn = engine.connect()
-        # drop the mock database if it exists
+        # drop mock database if it exists
         try:
-            conn.execute(text(f"DROP DATABASE IF EXISTS {mock_db_name}"))
-        except Exception as e:
-            print(e)
-        # create the mock database
-        try:
-            conn.execute(text(f"CREATE DATABASE {mock_db_name}"))
+            conn.execute(text(f"DROP DATABASE IF EXISTS {MOCK_DB_NAME}"))
         except Exception as e:
             print(e)
 
+        # create mock database
+        try:
+            conn.execute(text(f"CREATE DATABASE {MOCK_DB_NAME}"))
+        except Exception as e:
+            print(e)
 
+    @classmethod
+    def tearDownClass(cls) -> None:
 
+        conn = ENGINE.connect()
+
+        # drop mock database
+        try:
+            conn.execute(text(f"DROP DATABASE {MOCK_DB_NAME}"))
+        except Exception as e:
+            print(e)
