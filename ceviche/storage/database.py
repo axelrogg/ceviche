57f99d5f9bef44558e951451e6037752e0235960
Axel <edgysquirrel@pm.me>
2021-08-15 13:39:20 +0200

    storage: Refactored Database class.
    1. DatabaseCredential dataclass was moved to its own file.
    2. Changed Database methods:
        - show_tables was renamed to tables.
        - gen_engine was renamed to engine and simplified.
        - __reflect_db return type was changed to Optional[DatabaseTables].
        - __set_db_url was simplified.

--- ceviche/storage/database.py
+++ ceviche/storage/database.py
@@ -1,19 +1,13 @@
 from typing import Optional
 
-from pydantic import BaseModel
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.util._collections import FacadeDict           # noqa
 
 from ceviche.storage import DatabaseTables, DatabaseUrl
 from ceviche.storage.ddriver import DatabaseDriver, DDriver
-
-
-class DatabaseCredential(BaseModel):
-    dbhost: str
-    dbname: str
-    dbpwrd: str
-    dbuser: str
+from ceviche.storage.credentials import DatabaseCredentials
 
 
 class Database:
@@ -21,47 +15,52 @@ class Database:
     def __init__(
             self,
             ddriver: DDriver,
-            credential: DatabaseCredential,
+            credentials: DatabaseCredentials,
             metadata: MetaData,
-            tables: Optional[DatabaseTables] = None,
+            tabls: Optional[DatabaseTables] = None,
             is_async: bool = False
     ) -> None:
         self.ddriver = ddriver
         self.metadata = metadata
-        self.tables = tables
+        self.tabls = tabls
         self.is_async = is_async
-        self.__dbname = credential.dbname
-        self.__dbhost = credential.dbhost
-        self.__dbpwrd = credential.dbpwrd
-        self.__dbuser = credential.dbuser
+        self.__dbname = credentials.name
+        self.__dbhost = credentials.host
+        self.__dbpwrd = credentials.pwrd
+        self.__dbuser = credentials.user
         self.__url = self.__set_db_url()
 
-    def show_tables(self) -> DatabaseTables:
-        if self.tables:
-            return self.tables
+    def tables(self) -> Optional[DatabaseTables]:
+        if self.tabls:
+            return self.tabls
         return self.__reflect_db()
 
-    def gen_engine(self, is_async: bool = False, override_url: Optional[DatabaseUrl] = None, **kwargs) -> Engine:
+    def engine(self, force_sync_engine: bool = False, override_url: Optional[DatabaseUrl] = None, **kwargs) -> Engine:
         url = override_url or self.__url
         engine = create_engine(url, **kwargs)
-        if is_async:
+        if self.is_async and not force_sync_engine:
             engine = create_async_engine(url, **kwargs)
         return engine
 
-    def __reflect_db(self) -> DatabaseTables:
+    def __reflect_db(self) -> Optional[DatabaseTables]:
         # reflection can only be performed on a non-async engine
-        if not self.is_async:
-            engine = self.gen_engine()
-        else:
+        engine = self.engine()
+
+        if self.is_async:
             # TODO: right now we generate a new url for a sync driver for postgresql.  This needs to be automatic
             # by mapping sync and async drivers to the corresponding client.
             tmp_url = self.__set_db_url(DatabaseDriver.psycopg2)
             # create a sync engine
-            engine = self.gen_engine(override_url=tmp_url)
+            engine = self.engine(force_sync_engine=True, override_url=tmp_url)
         result = self.metadata.reflect(bind=engine)
-        return dict(result)
+
+        if isinstance(result, FacadeDict):
+            return dict(result)
+        return None
 
     def __set_db_url(self, override_driver: Optional[DatabaseDriver] = None) -> DatabaseUrl:
         client = self.ddriver.client
-        driver = override_driver.name or self.ddriver.driver
+        driver = self.ddriver.driver
+        if override_driver:
+            driver = override_driver.name
         return f"{client}+{driver}://{self.__dbuser}:{self.__dbpwrd}@{self.__dbhost}/{self.__dbname}"
