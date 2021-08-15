ee89337277b48e6ed8d684308ee3dbda44917f95
Axel <edgysquirrel@pm.me>
2021-08-15 13:45:04 +0200

    db: Reflect changes made in ceviche's init file and Database class.

--- ceviche/db.py
+++ ceviche/db.py
@@ -1,7 +1,13 @@
 from os import getenv
 from sqlalchemy import MetaData
-from ceviche.storage.database import Database, DatabaseCredential
-from ceviche.storage.ddriver import DatabaseClient, DatabaseDriver, DDriver
+from ceviche import (
+    Database,
+    DatabaseClient,
+    DatabaseCredentials,
+    DatabaseDriver,
+    DDriver,
+    get_default_tables,
+)
 
 
 dbhost = getenv("POSTGRES_HOST")
@@ -11,10 +17,10 @@ dbuser = getenv("POSTGRES_USER")
 
 
 ddriver = DDriver(client=DatabaseClient.postgresql, driver=DatabaseDriver.asyncpg)
-cred = DatabaseCredential(dbhost=dbhost, dbname=dbname, dbpwrd=dbpwrd, dbuser=dbuser)
+cred = DatabaseCredentials(host=dbhost, name=dbname, pwrd=dbpwrd, user=dbuser)
 
 _meta = MetaData()
 
-_db = Database(ddriver, cred, _meta, tables=None, is_async=True)
-db_engine = _db.gen_engine()
-db_tables = _db.show_tables()
+_db = Database(ddriver, cred, _meta, tabls=get_default_tables(_meta), is_async=True)
+db_engine = _db.engine()
+db_tables = _db.tables()
