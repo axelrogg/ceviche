8ec825d114543687618376beaafb4f8866186600
Axel <edgysquirrel@pm.me>
2021-08-15 13:44:03 +0200

    ceviche: Added most important imports to init file.

--- ceviche/__init__.py
+++ ceviche/__init__.py
@@ -1,1 +1,4 @@
-__version__ = '0.1.0'
+from ceviche.storage.credentials import DatabaseCredentials
+from ceviche.storage.database import Database
+from ceviche.storage.ddriver import DatabaseClient, DatabaseDriver, DDriver
+from ceviche.storage.models import get_default_tables
