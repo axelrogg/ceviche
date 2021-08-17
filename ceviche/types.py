749ac7e3915d5e43c6a7f8c07e0c569eacfa42ad
Axel Rodríguez Chang <edgysquirrel@pm.me>
2021-08-17 23:56:28 +0200

    types: Added UserID pydantic model type.

--- ceviche/types.py
+++ ceviche/types.py
@@ -1,5 +1,21 @@
 from typing import Dict
+
+from pydantic import BaseModel, root_validator
 from sqlalchemy import Table
+from uuid import UUID
+
 
 DatabaseUrl = str
 DatabaseTables = Dict[str, Table]
+
+
+class UserID(BaseModel):
+    value: str
+
+    @classmethod
+    @root_validator(pre=True)
+    def check_user_id_is_valid_uuid_str(cls, values):
+        try:
+            UUID(values.get("user_id"))
+        except ValueError:
+            raise ValueError("Invalid UserID format.")
